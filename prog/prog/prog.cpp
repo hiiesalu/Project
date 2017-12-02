87d5e9c64ff5b8830d71f0f35e3cb2f5d475eafc
hiiesalu <kristina_0897@mail.ru>
2017-12-02 13:18:09 +0300

    Made the base

---
+++ prog/prog/prog.cpp
@@ -0,0 +1,41 @@
+//פאיכ program.cpp
+#include "stdafx.h"
+#include <iostream>
+#include "funcs.h" 
+
+using namespace std;
+
+int main()
+{
+	int ch;
+	double a;
+	double b;
+
+	cout<<"Enter 1st number: ";
+	cin>>a;
+	cout<<"Enter 2nd number: ";
+	cin>>b;
+
+	cout<<"Enter function:\n1-summation;\n2-subtraction;\n3-multiplication;\n4-division.\n";
+	cin>>ch;
+	cout<<"You have selected function number "<<ch<<endl;
+
+	switch (ch){
+	case 1:
+cout <<"Summation is being developed"<<endl;
+	break;
+	case 2:
+cout << "Subtraction is being developed"<<endl;
+    break;
+	case 3:
+cout << "Multiplication is being developed"<<endl;
+    break;
+	case 4:
+cout << "Division is being developed"<<endl;
+    break;
+	default:
+		cout<<"error"<<endl;
+	}
+	system ("pause");
+ return 0;
+}
